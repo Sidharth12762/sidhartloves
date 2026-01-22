e0b3fdff54d9d761d14f93668045bbcf06087eec
Sidharth12762 <sidharthmenon170307@gmail.com>
2026-01-22 11:53:19 +0530

    Add Bitwise operations example in C

---
+++ Bitwise operation.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int x = 5, y = 3;
+    printf("AND =%d\n",x&y);
+    printf("OR =%d\n",x|y);
+    printf("Left Shift =%d\n",x<<y);
+    printf("Right Shift =%d\n",x>>y);
+
+    return 0;
+}
