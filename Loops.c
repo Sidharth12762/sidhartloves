15703fb32586a8ed04a2d95bb1c74fc43bb703bd
Sidharth12762 <sidharthmenon170307@gmail.com>
2026-01-22 13:42:05 +0530

    Add loops examples using for, while, and do-while

---
+++ Loops.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+    int i = 1;
+    for(i=1;i<=10;i++)
+    printf("%d\n",i);
+
+    return 0;
+}
+
+
+
+#include<stdio.h>
+int main(){
+int i = 1;
+while(i <= 5) {
+    printf("%d ", i);
+    i++;
+}
+    return 0;
+}
+
+
+
+
+
+
+#include<stdio.h>
+int main(){
+    int i = 1;
+    do{
+        printf("%d", i);
+        
+    }
+    while(i<=10);
+    i++;
+
+    return 0;
+}
